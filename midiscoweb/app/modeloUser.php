ea6dcdc9e7df36598ec02966bec6e34bbf8dfbfb
MonicaBG2407 <72247579+MonicaBG2407@users.noreply.github.com>
2021-01-05 00:00:10 +0100

    Update modeloUser.php

--- midiscoweb/app/modeloUser.php
+++ midiscoweb/app/modeloUser.php
@@ -1,12 +1,12 @@
 <?php 
 include_once 'config.php';
 /* DATOS DE USUARIO
-• Identificador ( 5 a 10 caracteres, no debe existir previamente, solo letras y números)
-• Contraseña ( 8 a 15 caracteres, debe ser segura)
-• Nombre ( Nombre y apellidos del usuario
-• Correo electrónico ( Valor válido de dirección correo, no debe existir previamente)
-• Tipo de Plan (0-Básico |1-Profesional |2- Premium| 3- Máster)
-• Estado: (A-Activo | B-Bloqueado |I-Inactivo )
+â€¢ Identificador ( 5 a 10 caracteres, no debe existir previamente, solo letras y nÃºmeros)
+â€¢ ContraseÃ±a ( 8 a 15 caracteres, debe ser segura)
+â€¢ Nombre ( Nombre y apellidos del usuario
+â€¢ Correo electrÃ³nico ( Valor vÃ¡lido de direcciÃ³n correo, no debe existir previamente)
+â€¢ Tipo de Plan (0-BÃ¡sico |1-Profesional |2- Premium| 3- MÃ¡ster)
+â€¢ Estado: (A-Activo | B-Bloqueado |I-Inactivo )
 */
 // Inicializo el modelo 
 // Cargo los datos del fichero a la session
@@ -15,8 +15,8 @@ function modeloUserInit(){
     /*
     $tusuarios = [ 
          "admin"  => ["12345"      ,"Administrado"   ,"admin@system.com"   ,3,"A"],
-         "user01" => ["user01clave","Fernando Pérez" ,"user01@gmailio.com" ,0,"A"],
-         "user02" => ["user02clave","Carmen García"  ,"user02@gmailio.com" ,1,"B"],
+         "user01" => ["user01clave","Fernando PÃ©rez" ,"user01@gmailio.com" ,0,"A"],
+         "user02" => ["user02clave","Carmen GarcÃ­a"  ,"user02@gmailio.com" ,1,"B"],
          "yes33" =>  ["micasa23"   ,"Jesica Rico"    ,"yes33@gmailio.com"  ,2,"I"]
         ];
     */
@@ -26,10 +26,10 @@ function modeloUserInit(){
     $_SESSION['tusuarios'] = $tusuarios;
    }
 
-      
+   
 }
 
-// Comprueba usuario y contraseña (boolean)
+// Comprueba usuario y contraseÃ±a (boolean)
 function modeloOkUser($user,$clave){
     if(isset($_SESSION['tusuarios'][$user])){
         if($_SESSION['tusuarios'][$user][0]==$clave){
@@ -42,7 +42,7 @@ function modeloOkUser($user,$clave){
 
 // Devuelve el plan de usuario (String)
 function modeloObtenerTipo($user){
-    return $_SESSION['tusuarios'][$user][3]; // Máster
+    return $_SESSION['tusuarios'][$user][3]; // MÃ¡ster
 }
 
 // Borrar un usuario (boolean)
@@ -50,7 +50,7 @@ function modeloUserDel($user){
     unset($_SESSION['tusuarios'][$user]);
     header("Location: index.php?orden=VerUsuarios");
 }
-// Añadir un nuevo usuario (boolean)
+// AÃ±adir un nuevo usuario (boolean)
 function modeloUserAdd($idUsuario,$datos){
     $_SESSION['tusuarios'][$idUsuario]=$datos;
     modeloUserSave(); 
@@ -64,8 +64,8 @@ function modeloUserUpdate ($userid,$userdat){
 
 // Tabla de todos los usuarios para visualizar
 function modeloUserGetAll (){
-    // Genero lo datos para la vista que no muestra la contraseña ni los códigos de estado o plan
-    // sino su traducción a texto
+    // Genero lo datos para la vista que no muestra la contraseÃ±a ni los cÃ³digos de estado o plan
+    // sino su traducciÃ³n a texto
     $tuservista=[];
     foreach ($_SESSION['tusuarios'] as $clave => $datosusuario){
         $tuservista[$clave] = [$datosusuario[1],
@@ -94,11 +94,9 @@ function validarId($idUsuario) {
 function existe($idUsuario){
     foreach ($_SESSION['tusuarios'] as $id => $datos){
         if($id ==$idUsuario){
-            echo 'user repetido ';
             return true;
         }
     }
-    
     return false;
 }
 
@@ -111,7 +109,6 @@ function validarContra($contra){
 
 function validarCorreo($correo,$idUsuario){
     if(!existeCorreo($correo)){
-        
             if(filter_var($correo, FILTER_VALIDATE_EMAIL)==true){
                 return true;
             }
@@ -122,11 +119,9 @@ function validarCorreo($correo,$idUsuario){
 function existeCorreo($correo){
     foreach ($_SESSION['tusuarios'] as $id => $datos){
         if( $datos[2]==$correo){
-            echo 'correo repetido ';
             return true;
         }
     }
-    
     return false;
 }
 
